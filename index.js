f9eb188712bf6aa86c05d07011728333773da5ac
Chase Groom <groom.chase@gmail.com>
2023-09-12 16:29:36 -0600

    changes

--- index.js
+++ index.js
@@ -2,19 +2,9 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const FTP = require("basic-ftp");
-const cron = require("cron");
+const fs = require("fs");
 
 dotenv.config();
-
-const app = express();
-const port = process.env.HTTP_PORT;
-
-app.use(express.static(path.join(process.cwd(), "dist")));
-
-app.get("/inventory.csv", (req, res) => res.sendFile(path.join(process.cwd(), "data/inventory.csv")));
-app.get("*", (req, res) => res.sendFile(path.join(process.cwd(), "dist/index.html")));
-
-app.listen(port, () => console.log(`http://localhost:${port}!`));
 
 const pullInventory = async () => {
 	const config = {
@@ -38,14 +28,29 @@ const pullInventory = async () => {
 	} catch (err) {
 		console.log(err);
 	}
-
 };
 
-const job = new cron.CronJob("0 */2 * * *", () => {
-	pullInventory();
-}, null, true, "America/Denver");
-job.start();
+const app = express();
+const port = process.env.HTTP_PORT;
 
-console.log("started");
+app.use(express.static(path.join(process.cwd(), "dist")));
 
-pullInventory();
+app.get("/inventory.csv", async (req, res) => {
+	// If inventory.csv does not exist, fetch a new file
+	if (!fs.existsSync(path.join(process.cwd(), "data/inventory.csv"))) {
+		await pullInventory();
+	}
+
+	// If inventory.csv is older than 2 hours, fetch a new file
+	const stats = fs.statSync(path.join(process.cwd(), "data/inventory.csv"));
+	if (Date.now() - Math.round(stats.mtimeMs) > 1000 * 60 * 60 * 2) {
+		await pullInventory();
+	}
+
+	res.sendFile(path.join(process.cwd(), "data/inventory.csv"));
+});
+
+app.get("*", (req, res) => res.sendFile(path.join(process.cwd(), "dist/index.html")));
+
+await pullInventory();
+app.listen(port, () => console.log(`http://localhost:${port}!`));
